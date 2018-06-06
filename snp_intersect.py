0c4cb2881083e35c698eeb3f7ec96f8c499bd3ea
Sean La <seanla1996@gmail.com>
2018-06-06 12:32:48 -0700

    added intersection script; not yet tested

---
+++ snp_intersect.py
@@ -0,0 +1,115 @@
+#/usr/bin/env python
+import sys
+import os
+import getopt
+
+def get_cytokine_paths(cytokine_dir):
+    cytokine_paths = []
+    for file in os.listdir(cytokine_dir):
+        if file.endswith(".assoc")
+            name = file.split('.')[0]
+            cytokine_paths.append((name,os.path.join(cytokine_dir,file))) 
+    return cytokine_paths
+
+def get_cytokine_snps(cytokine_paths,alpha):
+    cytokine_snps = {} 
+    for (name,path) in cytokine_paths: 
+        snps = []
+        with open(path,'r') as file:
+            for line in file:
+                tokens = line.split(' ')
+                if tokens[8] != "NA" and tokens[8] != "P" and float(tokens[8]) <= alpha:
+                    snps.append(tokens[1])
+        cytokine_snps[name] = snps
+    return cytokine_snps
+
+def get_survival_snps(survival_path):
+    snps = []
+    with open(survival_path,'r') as file:
+        for line in file:
+            tokens = line.split(' ')
+            if tokens[8] != "NA" and tokens[8] != "P" and float(tokens[8]) <= alpha:
+                snps.append(token[1])
+    return snps
+
+def intersect_snps(cytokine_snps,survival_snps):
+    intersection = {}
+    for cytokine in cytokine_snps:
+        snps = []
+        for snp in survival_snps:
+            if snp in cytokine_snps[cytokine]
+                snps.append(snp)
+        intersection[cytokine] = snps
+    return intersection
+
+def output_snps(intersected_snps,output):
+    path = output + ".tsv"
+    with open(path,'w') as file:
+        for cytokine in intersected_snps:
+            snps = ""
+            for snp in intersected_snps[cytokine]:
+                snps = snps + "snp,"
+            snps.rstrip(',')
+            file.write(cytokine + "\t" + "snps")
+
+def main(argv):
+    help_message = ""
+    usage_message = ""
+    options = "hs:c:o:a:"
+
+    try:
+        opts,args = getopt.getopt(argv[1:],options)
+    except getopt.GetoptError:
+        print("Error: unable to read command line arguments.")
+        sys.exit(1)
+
+    if len(argv) == 1:
+        print(help_message)
+        print(usage_message)
+        sys.exit(1)    
+    
+    survival_path = None
+    cytokine_dir = None
+    output = None
+    alpha = None
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print(help_message)
+            print(usage_message)
+        elif opt == '-s':
+            survival_path = arg
+        elif opt == '-c':
+            cytokine_dir = arg
+        elif opt == '-o':
+            output = arg
+        elif opt == '-a':
+            alpha = arg
+
+    opts_incomplete = False
+
+    if survival_path is None:
+        print("Error: please provide your survival SNP association file.")
+        opts_incomplete = True
+    if cytokine_dir is None:
+        print("Error: please provide the directory containing your cytokine SNP association files.")
+        opts_incomplete = True
+    if output is None:
+        print("Error: please provide an output prefix.")
+        opts_incomplete = True
+    if alpha is None:
+        print("Error: please provide an alpha value for the P-value cutoff.")
+        opts_incomplete = True
+    
+    if opts_incomplete:
+        print(usage_message)
+        sys.exit(1)
+
+    cytokine_paths = get_cytokine_paths(cytokine_dir)
+    cytokine_snps = get_cytokine_snps(cytokine_paths,alpha)
+    survival_snps = get_survival_snps(survival_path)
+    intersected_snps = intersect_snps(cytokine_snps,survival_snps)
+    output_snps(intersected_snps,output)
+    
+if __name__ == "__main__":
+    main(sys.argv)
